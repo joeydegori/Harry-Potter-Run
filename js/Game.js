4af6f70cff7a097df0f4049e475bd672ba6beab6
joeydegori <96920177+joeydegori@users.noreply.github.com>
2022-02-19 14:58:44 -0500

    Background set

---
+++ js/Game.js
@@ -0,0 +1,38 @@
+class Game {
+    constructor(context){
+        this.context = context;
+        this.forest = new Image(); 
+        this.forest.src = './images/forest.png';
+        this.i = 0; 
+        this.j = canvas.width; 
+    }
+
+    drawBackground = () => {
+    this.context.clearRect(0, 0, canvas.width, canvas.height);
+        
+        this.i--; 
+
+        if (this.i <= -canvas.width){
+            this.i = canvas.width; 
+        }
+
+        this.j--; 
+        if (this.j <= -canvas.width){
+            this.j = canvas.width; 
+        }
+
+    this.context.drawImage(this.forest, this.i, 0, canvas.width, canvas.height); 
+    this.context.drawImage(this.forest, this.j, 0, canvas.width, canvas.height); 
+    } 
+
+    drawEverything = () => {
+        //frame
+        let animation = requestAnimationFrame(this.drawEverything);
+        this.drawBackground();
+
+
+    }
+}
+
+ 
+
---
+++ js/index.js
@@ -0,0 +1,19 @@
+window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
+      startGame();
+    };
+  
+    function startGame() {
+      const canvas = document.getElementById('canvas')
+      const context = canvas.getContext('2d');
+  
+      const game = new Game(context)
+      game.drawEverything(); 
+  
+        document.addEventListener('keydown', event => {
+        game.character.controls(event.code)
+      })
+    }
+}
+  
+  
